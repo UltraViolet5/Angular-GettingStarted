b14f8df49ee14913f50898a1eb5702f6f13072e0
UltraViolet5 <70701763+UltraViolet5@users.noreply.github.com>
2022-03-12 18:30:51 +0100

    my first component

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:
+    `
+      <div>
+        <h1>{{pageTitle}}</h1>
+        <div>My First App Component</div>
+      </div>
+    `
+  
+    
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = "ACME PRODUCT MANAGMENT" ;
 }
